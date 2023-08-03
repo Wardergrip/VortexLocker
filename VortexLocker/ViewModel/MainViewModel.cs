2a6bab12efb380db67e2672d5143a9b8af7b45f7
Wardergrip <rei.messely@gmail.com>
2023-08-03 23:50:59 +0200

    feat: MainViewModel

---
+++ VortexLocker/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VortexLocker.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
