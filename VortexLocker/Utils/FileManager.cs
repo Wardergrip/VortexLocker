3adcabbcac30bdc16aec665db66e6c1697c45d18
Wardergrip <rei.messely@gmail.com>
2023-08-05 23:16:53 +0200

    feat: setup filemanager

---
+++ VortexLocker/Utils/FileManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace VortexLocker.Utils
+{
+    public class FileManager
+    {
+        public string RootDirectory { get; private set; }
+        public FileManager(string directory)
+        {
+            RootDirectory = directory;
+            //Directory.GetDirectories(".");
+        }
+    }
+}
