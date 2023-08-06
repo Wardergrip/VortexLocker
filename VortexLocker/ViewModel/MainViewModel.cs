21988953730045a99094a0442dbafe5a761faca8
Wardergrip <rei.messely@gmail.com>
2023-08-06 23:33:02 +0200

    feat: display all files from one dir

--- VortexLocker/ViewModel/MainViewModel.cs
+++ VortexLocker/ViewModel/MainViewModel.cs
@@ -2,11 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using VortexLocker.Utils;
 
 namespace VortexLocker.ViewModel
 {
     public class MainViewModel : ObservableObject
     {
+        // This needs to be stored in a file and be configured from within.
+        private FileManager _fileManager = new FileManager("E:\\Git\\VortexLocker\\VortexLocker");
+
         public MainViewModel()
         {
 
@@ -14,7 +18,12 @@ namespace VortexLocker.ViewModel
 
         public string GitUsername 
         { 
-            get { return Utils.CmdHelper.GetGitUsername(); } 
+            get { return CmdHelper.GetGitUsername(); } 
+        }
+
+        public List<string> Directories
+        {
+            get { return _fileManager.GetAllDirectories(); }
         }
     }
 }
