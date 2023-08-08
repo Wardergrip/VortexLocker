78d63cf089e41cc652d390dc30a54b236fca59cc
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2023-08-08 21:21:55 +0200

    feat: tabs & overviewpage

--- VortexLocker/ViewModel/MainViewModel.cs
+++ VortexLocker/ViewModel/MainViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
 using VortexLocker.Utils;
+using VortexLocker.View;
 
 namespace VortexLocker.ViewModel
 {
@@ -11,8 +12,14 @@ namespace VortexLocker.ViewModel
         // This needs to be stored in a file and be configured from within.
         private FileManager _fileManager;
 
+        public RelayCommand OpenGithubReposCommand { get; private set; }
+        public OverviewPage OverviewPage { get; private set; } = new OverviewPage();
+        public string WindowTitle { get; private set; }
+
         public MainViewModel()
         {
+            WindowTitle = $"VortexLocker v0.1 | User: {GitUsername}";
+            OpenGithubReposCommand = new RelayCommand(OpenGithubRepos);
             _fileManager = new FileManager(App.FileArg);
             //LockLogger.Init(App.FileArg);
         }
@@ -26,5 +33,7 @@ namespace VortexLocker.ViewModel
         {
             get { return _fileManager.GetAllAbsoluteDirectories(); }
         }
+
+        private void OpenGithubRepos() => Process.Start(new ProcessStartInfo("https://github.com/Wardergrip/VortexLocker"));
     }
 }
---
+++ VortexLocker/ViewModel/OverviewVM.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VortexLocker.ViewModel
+{
+    public class OverviewVM : ObservableObject
+    {
+    }
+}
