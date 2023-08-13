b4e4546c8bea86eba21f99f5f925323a5c975006
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2023-08-13 21:20:07 +0200

    feat: terminal logging

--- VortexLocker/ViewModel/OverviewVM.cs
+++ VortexLocker/ViewModel/OverviewVM.cs
@@ -1,8 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace VortexLocker.ViewModel
 {
     public class OverviewVM : ObservableObject
     {
+        public RelayCommand TestButtonCommand { get; set; }
+        public static ObservableCollection<string> TerminalEntries { get; private set; }
+
+        public OverviewVM()
+        {
+            TestButtonCommand = new RelayCommand(TestButton);
+            TerminalEntries = new();
+            LogOnTerminal("TERMINAL LOG");
+            LogOnTerminal("============");
+            LogOnTerminal("");
+        }
+
+        private void TestButton()
+        {
+            LogOnTerminal("A new log msg");
+        }
+
+        public static void LogOnTerminal(string message) 
+        {
+            TerminalEntries.Add($"{DateTime.Now} | {message}");
+        }
+
+        public static void SaveTerminalLog(string filepath)
+        {
+            StreamWriter writer = new(filepath);
+
+            foreach (var entry in TerminalEntries)
+            {
+                writer.WriteLine(entry);
+            }
+        }
     }
 }
