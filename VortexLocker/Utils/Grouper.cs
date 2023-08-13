a2f926690ba2965796dec4a9786569fc62a2cb8d
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2023-08-13 22:58:19 +0200

    feat: grouping (changelists) fundamentals

---
+++ VortexLocker/Utils/Grouper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VortexLocker.Utils
+{
+    public class Grouper
+    {
+        public List<List<string>> Groups { get; private set; }
+        public int GroupAmount { get { return Groups.Count; } }
+
+        public event Action OnGroupsChanged;
+
+        public Grouper()
+        {
+            Groups = new()
+            {
+                new()
+            };
+        }
+
+        /// <summary>
+        /// If groupnr >= GroupAmount, new group is made.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="groupNr"></param>
+        /// <exception cref="Exception">Invalid groupnr</exception>
+        public void AddPathToGroup(string path, int groupNr = 0)
+        {
+            if (groupNr < 0)
+            {
+                throw new Exception($"Trying to add {path} to a negative group nr ({groupNr})");
+            }
+            if (GroupAmount > groupNr)
+            {
+                Groups[0].Add(path);
+            }
+            else
+            {
+                Groups.Add(new() { path });
+            }
+
+            OnGroupsChanged?.Invoke();
+        }
+
+        public bool RemovePathFromGroup(string path, int? groupNr = null) 
+        {
+            // Caller doesn't know groupnr, so we figure it out
+            if (!groupNr.HasValue)
+            {
+                foreach (var grp in Groups)
+                {
+                    int idx = grp.FindIndex(x => x == path);
+                    if (idx < 0)
+                    {
+                        continue;
+                    }
+                    groupNr = idx;
+                }
+                // Check if we assigned a value, if not it means we did not find it
+                if (!groupNr.HasValue)
+                {
+                    return false;
+                }
+            }
+
+            Groups[groupNr.Value].Remove(path);
+            if (Groups[groupNr.Value].Count == 0)
+            {
+                // Rearrange groups, clean up this one and move stuff in higher nr groups if applicable
+            }
+
+            OnGroupsChanged?.Invoke();
+            return true;
+        }
+
+        public bool RemoveAllPathsFromGroup(int groupnr)
+        {
+            if (GroupAmount < groupnr)
+            {
+                return false;
+            }
+
+            Groups[groupnr].Clear();
+            // Rearrange groups, clean up this one and move stuff in higher nr groups if applicable
+
+            OnGroupsChanged?.Invoke();
+            return true;
+        }
+    }
+}
--- VortexLocker/ViewModel/OverviewVM.cs
+++ VortexLocker/ViewModel/OverviewVM.cs
@@ -3,13 +3,18 @@ using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Collections.Generic;
+using VortexLocker.Utils;
 
 namespace VortexLocker.ViewModel
 {
     public class OverviewVM : ObservableObject
     {
+        private Grouper _grouper = new();
         public RelayCommand TestButtonCommand { get; set; }
         public static ObservableCollection<string> TerminalEntries { get; private set; }
+        public ObservableCollection<string> GroupsDisplay { get; private set; }
+        public string SelectedGroupItem { get; set; }
 
         public OverviewVM()
         {
@@ -18,13 +23,35 @@ namespace VortexLocker.ViewModel
             LogOnTerminal("TERMINAL LOG");
             LogOnTerminal("============");
             LogOnTerminal("");
+
+            GroupsDisplay = new()
+            {
+                "RootGroup",
+                "   GROUP 0:"
+            };
+            _grouper.OnGroupsChanged += UpdateGroupsDisplay;
         }
 
         private void TestButton()
         {
-            LogOnTerminal("A new log msg");
+            LogOnTerminal($"{SelectedGroupItem}");
         }
 
+        private void UpdateGroupsDisplay()
+        {
+            GroupsDisplay.Clear();
+            GroupsDisplay.Add("RootGroup");
+            for (int i = 0; i < _grouper.Groups.Count; ++i) 
+            {
+                GroupsDisplay.Add($"    GROUP {i}:");
+                foreach (string path in _grouper.Groups[i])
+                {
+                    GroupsDisplay.Add($"        {path}");
+                }
+            }
+        }
+
+        #region Terminal
         public static void LogOnTerminal(string message) 
         {
             TerminalEntries.Add($"{DateTime.Now} | {message}");
@@ -39,5 +66,6 @@ namespace VortexLocker.ViewModel
                 writer.WriteLine(entry);
             }
         }
+        #endregion
     }
 }
